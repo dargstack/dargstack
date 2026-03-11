86c05a06e3af1af0d63953899fb97730a9fb93cd
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-03-11 13:05:35 +0100

    fix(version): fall back to build info

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,8 +1,23 @@
 package version
 
+import "runtime/debug"
+
 // Set via ldflags at build time by goreleaser.
 var (
 	Version = "dev"
 	Commit  = ""
 	Date    = ""
 )
+
+func init() {
+	if Version != "dev" {
+		return
+	}
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	if v := info.Main.Version; v != "" && v != "(devel)" {
+		Version = v
+	}
+}
